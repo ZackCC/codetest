73ae31a40e6cd22077fcf715e37763b72b6118d3
Zack CC <zack@zack.cc>
2019-02-19 16:09:04 -0800

    broke out javascript into app.js, loaded in header for UI.

---
+++ assets/app.js
@@ -0,0 +1,32 @@
+/* tap the console
+  (function () {
+   console.log("Hello Browser World!")
+  }());
+*/
+  function randomclick() {
+    //console.log("Somebody wanted a random fact...")
+   fetch('http://localhost:3000/fact/random')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(randomfact) {
+    const resultingfact = JSON.stringify(randomfact)
+    console.log(resultingfact);
+    document.getElementById("randomfactsection").innerHTML = resultingfact;
+  });
+  }
+  function drawbuttons() {
+    fetch('http://localhost:3000/fact/')
+   .then(function(response) {
+     return response.json();
+   })
+   .then(function(eachfact) {
+     const eachfact = JSON.stringify(eachfact)
+     console.log(eachfact);
+     document.getElementById("randomfactsection").innerHTML = eachfact;
+   });
+   }
+
+    for ( let factnumber = 0; factnumber < cars.length; factnumber++) {
+      text += cars[i] + "<br>";
+    }
